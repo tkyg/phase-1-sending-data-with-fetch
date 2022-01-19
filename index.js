6a5ea7b270415bef18d2326bdabe85628351471e
tkyg <80114433+tkyg@users.noreply.github.com>
2022-01-19 10:44:53 -0500

    commit tree 9f4333f3ce790a971a7f3da73e27b4f50fba65b5

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(userName, userEmail){
+  return fetch("http://localhost:3000/users",{
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name: userName,
+    email: userEmail,
+  })
+})
+.then(response => response.json())
+.then(json => {
+  const newItemId = json.id;
+  const p = document.createElement('p');
+  p.textContent = newItemId;
+  const body = document.querySelector('body');
+  body.append(p);
+})
+.catch(someError => {
+  const p = document.createElement('p');
+  p.textContent = someError.message;
+  const body = document.querySelector('body');
+  body.append(p);
+})
+}
+
